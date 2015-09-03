d58222251c82c825074b5c6c1ac7d0c59ad78719
finger92 <zhouyi920521@gmail.com>
2015-09-03 23:12:55 +0000

    add exception handle

--- msgclient.py
+++ msgclient.py
@@ -5,12 +5,16 @@
 import socket
 import sys
 
+if len(sys.argv) <= 2:
+    print('ERROR no enough parameters');
+    sys.exit(-1);
+
 host = "finger92.koding.io";
 portnum = 5555;
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM);   #get socket
 sock.connect((host,portnum));   #get connetion
-sock.send(sys.argv[1]+'\n'+sys.argv[2]); #send messages
+sock.send(sys.argv[len(sys.argv)-2]+'\n'+sys.argv[len(sys.argv)-1]); #send messages
 sock.shutdown(socket.SHUT_WR);    #shutdown socket before close
 sock.close();   #close socket
 
